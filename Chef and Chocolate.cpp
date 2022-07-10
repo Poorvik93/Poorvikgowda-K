ffc89f9544da21d02c59c7333783b66c6ee4652e
Poorvikgowda K <108992627+Poorvik93@users.noreply.github.com>
2022-07-10 13:34:35 +0530

    Create Chef and Chocolate.cpp

---
+++ Chef and Chocolate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z,amount,ans;
+        cin>>x>>y>>z;
+
+     if( amount=5*x +10*y){
+         ans=amount/z;
+        cout<<ans<<endl;
+        }
+        // else{
+        // cout<<ans<<endl;
+        // }
+     
+    }
+ return 0;   
+}
